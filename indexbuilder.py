e45fcea67e07fa2ca4300d65f15b7f61ecf4d03b
Z1R343L <jan2705g@gmail.com>
2021-08-24 01:53:55 +0200

    update

--- indexbuilder.py
+++ indexbuilder.py
@@ -1,8 +1,9 @@
 import asyncio
+from os.path import exists
 
 from glob import glob
 import httpx
-from dill import dump
+from dill import dump, load
 
 async def main():
     mons = {}
@@ -15,23 +16,41 @@ async def main():
     for rarity in ['common', 'uncommon', 'rare', 'legendary']:
         mons[rarity] = []
 
+    if not exists('cache.dill'):
+        cache = {}
+        cache['species'] = {}
+        cache['chain'] = {}
+        for mon in mons['regular']:
+            print(mon)
+            async with httpx.AsyncClient() as client:
+                resp = await client.get(f'https://pokeapi.co/api/v2/pokemon-species/{mon}')
+            mon_json = resp.json()
+            async with httpx.AsyncClient() as client:
+                resp = await client.get(mon_json['evolution_chain']['url'])
+            chain_json = resp.json()
+            cache['species'][mon] = mon_json
+            cache['chain'][mon] = chain_json
+            with open('cache.dill', 'wb') as f:
+                dump(cache, f)
+    else:
+        print("cahe found!")
+
+
+    with open('cache.dill', 'rb') as f:
+        cache = load(f)
     for mon in mons['regular']:
         print(mon)
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(f'https://pokeapi.co/api/v2/pokemon-species/{mon}')
-        mon_json = resp.json()
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(mon_json['evolution_chain']['url'])
-        chain_json = resp.json()
-        if mon_json['is_legendary'] or mon_json['is_mythical']:
+        if cache['species'][mon]['is_legendary'] or cache['species'][mon]['is_mythical']:
             mons['legendary'].append(mon)
-        elif not mon_json['evolves_from_species'] is None:
-            if len(chain_json['chain']['evolves_to']) == 0:
+        elif cache['species'][mon]['evolves_from_species'] is not None:
+            print(len(cache['chain'][mon]['chain']['evolves_to']))
+            print(str(cache['chain'][mon]))
+            if cache['chain'][mon]['chain']['evolves_to'][len(cache['chain'][mon]['chain']['evolves_to'])]:
                 mons['rare'].append(mon)
             else:
                 mons['uncommon'].append(mon)
         else:
-            if len(chain_json['chain']['evolves_to']) == 0:
+            if len(cache['chain'][mon]['chain']['evolves_to']) == 0:
                 mons['uncommon'].append(mon)
             else:
                 mons['common'].append(mon)
